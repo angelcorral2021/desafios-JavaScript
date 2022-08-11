38b669026132ce9560daacb9b320292fec824f9b
Angel Corral <82479794+angelcorral2021@users.noreply.github.com>
2022-08-10 20:28:26 -0400

    Ejercicio 18

---
+++ Ejercicio18.js
@@ -0,0 +1,10 @@
+/**Escriba una funcion que reciba dos date como parametros y retorne el numero de dias 
+ * entre esos date
+ */
+
+const dia1 = new Date("2022-04-01");
+const dia2 = new Date("2022-06-02");
+
+let resultado = dia2.getTime() - dia1.getTime();
+console.log(resultado)
+console.log(Math.round(resultado/ (1000*60*60*24)));
