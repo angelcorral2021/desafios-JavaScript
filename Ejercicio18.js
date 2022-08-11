5f6682af45d07479765a03ada44f43b03180f12d
Angel Corral <82479794+angelcorral2021@users.noreply.github.com>
2022-08-10 20:48:57 -0400

    Agregando archivo gitignore

--- Ejercicio18.js
+++ Ejercicio18.js
@@ -2,9 +2,15 @@
  * entre esos date
  */
 
-const dia1 = new Date("2022-04-01");
+const moment = require("moment");
+
+const dia1 = new Date("2022-05-01");
 const dia2 = new Date("2022-06-02");
 
 let resultado = dia2.getTime() - dia1.getTime();
 console.log(resultado)
 console.log(Math.round(resultado/ (1000*60*60*24)));
+
+let prueba = moment(dia1).subtract(1,'day')
+
+console.log(moment(prueba).format("MMM Do YY") )
